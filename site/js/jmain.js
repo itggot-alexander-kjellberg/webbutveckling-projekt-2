7030eec755f86e7d82e2a4003d6cf5aa59978b22
itggot-alexander-kjellberg <alexander.kjellberg2@elev.ga.ntig.se>
2019-01-24 15:10:09 +0100

    fixade så att man inte kunde skrolla åt sidan. Fixade javascripten för flinga samt fixede position fixed för skuggan i desktop

--- site/js/jmain.js
+++ site/js/jmain.js
@@ -1,3 +1,5 @@
+// GRANEN
+
 function visaCirkela(event){
     let host = document.querySelector("#julgranstopp")
     host.classList.toggle("host")
@@ -16,8 +18,26 @@ function visaHundraprocent(event){
 }
 
 
-function visaTjugoprocent(event){
+
+
+// FLINGA
+
+function visaTjugoprocenta(event){
     let yst = document.querySelector("#tjugoprocenta")
     yst.classList.toggle("yst")
+}
 
+function visaFemtioprocenta(event){
+    let yst = document.querySelector("#femtioprocenta")
+    yst.classList.toggle("yst")
+}
+
+function visaSjuttioprocenta(event){
+    let yst = document.querySelector("#sjuttioprocenta")
+    yst.classList.toggle("yst")
+}
+
+function visaHundraprocenta(event){
+    let yst = document.querySelector("#hundraprocenta")
+    yst.classList.toggle("yst")
 }
